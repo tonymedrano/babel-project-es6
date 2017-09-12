6fed87ebcaca351230fdb902095373c85c1d5a87
Tony Medrano <info@tonymdedrano.com>
2017-09-12 09:51:04 +0200

    Kick start project ready for coding

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,17 +1,28 @@
-'use strict';
+"use strict";
 
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Main = function () {
+    function Main(name) {
+        _classCallCheck(this, Main);
+
+        this.name = name;
+        this.run();
     }
-    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return monthlyPayment;
-};
 
-document.getElementById('calcBtn').addEventListener('click', function () {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-});
+    _createClass(Main, [{
+        key: "run",
+        value: function run() {
+            var div = document.createElement("div");
+            var content = document.createTextNode("Running " + this.name + "...");
+            div.appendChild(content);
+            document.body.appendChild(div);
+        }
+    }]);
+
+    return Main;
+}();
+
+var app = new Main("God");
--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-var calculateMonthlyPayment = function (principal, years, rate) {
-    if (rate) {
-        var monthlyRate = rate / 100 / 12;
+class Main {
+    constructor (name) {
+        this.name = name
+        this.run()
     }
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
-};
+    run () {
+        let div = document.createElement("div"); 
+        let content = document.createTextNode(`Running ${ this.name }...`); 
+        div.appendChild(content);
+        document.body.appendChild(div)
+    }
+}
 
-document.getElementById('calcBtn').addEventListener('click', function () {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
-});
+let app = new Main("God")
